a34da364ddf7275ccd0d0088236d058cd44dfa5f
kenziebottoms <kenziebottoms@gmail.com>
2018-08-01 13:28:05 -0500

    fixed palindrome fn

--- E04.java
+++ E04.java
@@ -3,16 +3,23 @@ import java.util.*;
 class E04 {
     public static void main(String[] args) {
         System.out.println(isPalindrome("hannah"));
+        System.out.println(isPalindrome("hannahs"));
+        System.out.println(isPalindrome(101));
+        System.out.println(isPalindrome(10));
     }
-    public static Boolean isPalindrome(String x) {
-        List<String> letters = Arrays.asList(x.split(""));
+    public static Boolean isPalindrome(String ex) {
+        List<String> letters = Arrays.asList(ex.split(""));
         List<String> srettel = new ArrayList<String>(letters);
         Collections.reverse(srettel);
         for (int i=0; i<letters.size()/2; i++) {
-            if (letters.get(i) == srettel.get(i)) {
+            if (!letters.get(i).equals(srettel.get(i))) {
                 return false;
             }
         }
         return true;
+    }
+    public static Boolean isPalindrome(int x) {
+        String ex = Integer.toString(x);
+        return isPalindrome(ex);
     }
 }
