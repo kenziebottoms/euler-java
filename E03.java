bb2c74941e3e520d6b81262a4716ce56b8d0b81b
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-08-01 11:30:51 -0500

    #3 completed

--- E03.java
+++ E03.java
@@ -3,19 +3,29 @@ import java.math.*;
 
 class E03 {
     public static void main(String[] args) {
-        System.out.println(primeFactors(BigInteger.valueOf(13195)));
+        Set<BigInteger> factors = primeFactors(new BigInteger("600851475143"));
+        System.out.println(Collections.max(new ArrayList<BigInteger>(factors)));
     }
-    public static List<BigInteger> primeFactors(BigInteger x) {
-        List<BigInteger> factors = new ArrayList<BigInteger>();
-        Integer i=2;
-        while (x.compareTo(BigInteger.valueOf(i)) >= 0) {
-            if (x.mod(BigInteger.valueOf(i)) == BigInteger.valueOf(0)) {
-                factors.add(BigInteger.valueOf(i));
-                x = x.divide(BigInteger.valueOf(i));
+    // returns the prime factors of x
+    public static Set<BigInteger> primeFactors(BigInteger x) {
+        Set<BigInteger> factors = new HashSet<BigInteger>();
+        BigInteger i = new BigInteger("2");
+        while (x.compareTo(i) >= 0) {
+            if (x.mod(i) == BigInteger.valueOf(0)) {
+                factors.add(i);
+                x = x.divide(i);
             } else {
-                i++;
+                i = i.add(BigInteger.valueOf(1));
             }
         }
         return factors;
+    }
+    // returns sum of set
+    public static BigInteger sum(Set<BigInteger> set) {
+        BigInteger sum = BigInteger.valueOf(0);
+        for (BigInteger x: set) {
+            sum = sum.add(x);
+        }
+        return sum;
     }
 }
