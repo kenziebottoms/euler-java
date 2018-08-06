cea443d2d2467b517b12bf07a34cfd8de456d374
kenziebottoms <kenziebottoms@gmail.com>
2018-08-06 16:38:13 -0500

    cleaned up #9

--- 6/E09.java
+++ 6/E09.java
@@ -4,15 +4,10 @@ import java.util.Arrays;
 class E09 {
     public static void main(String[] args) {
         int[] finalTrip = new int[3];
-        for (int j=4; j<1000; j++) {
+        for (int j=4; j<500; j++) {
             for (int i=3; i<j; i++) {
                 double k = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2));
-                int[] trip = {i, j, (int) k};
-                if (
-                    k == (double) Math.floor(k) &&
-                    i+j+(int)k == 1000 &&
-                    isPythy(trip)
-                ) {
+                if ((double) Math.floor(k) == k && (int) k + i + j == 1000) {
                     finalTrip[0] = i;
                     finalTrip[1] = j;
                     finalTrip[2] = (int) Math.floor(k);
@@ -20,10 +15,7 @@ class E09 {
                 }
             }
         }
-        long sum = (long) finalTrip[0]*finalTrip[1]*finalTrip[2];
+        long sum = finalTrip[0]*finalTrip[1]*finalTrip[2];
         System.out.println(sum);
-    }
-    public static Boolean isPythy(int[] trip) {
-        return ((int) Math.pow(trip[0], 2) + (int)Math.pow(trip[1], 2)) == (int) Math.pow(trip[2], 2);
     }
 }
