509cf0ee9fdb804143fbc0d10e5577fb28a4441f
kenziebottoms <kenziebottoms@gmail.com>
2018-08-06 13:54:21 -0500

    #9 solved

--- 6/E09.java
+++ 6/E09.java
@@ -1,9 +1,32 @@
 import java.lang.Math;
+import java.util.Arrays;
 
 class E09 {
     public static void main(String[] args) {
-        int[] trip = {3, 4, 5};
-        System.out.println(isPythy(trip));
+        int i, j, k;
+        int[] finalTrip = new int[3];
+        i = 3;
+        k = 5;
+        for (j=4; j<1000; j++) {
+            for (i=3; i<j; i++) {
+                double kDubs = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2));
+                int[] trip = {i, j, (int) kDubs};
+                if (
+                    kDubs == (double) Math.floor(kDubs) &&
+                    i+j+(int)kDubs == 1000 &&
+                    isPythy(trip)
+                ) {
+                    k = (int) Math.floor(kDubs);
+                    finalTrip[0] = i;
+                    finalTrip[1] = j;
+                    finalTrip[2] = k;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(finalTrip));
+        long sum = (long) finalTrip[0]*finalTrip[1]*finalTrip[2];
+        System.out.println(sum);
     }
     public static Boolean isPythy(int[] trip) {
         return ((int) Math.pow(trip[0], 2) + (int)Math.pow(trip[1], 2)) == (int) Math.pow(trip[2], 2);
--- 6/README.md
+++ 6/README.md
@@ -10,6 +10,6 @@ java E01
 |    |    |    |    |    |
 | -- | -- | -- | -- | -- |
 | :heavy_check_mark: [1](E01.java) | :heavy_check_mark: [2](E02.java) | :heavy_check_mark: [3](E03.java) | :heavy_check_mark: [4](E04.java) | :heavy_check_mark: [5](E05.java) |
-| :heavy_check_mark: [6](E06.java) | :heavy_check_mark: [7](E07.java) | :heavy_check_mark: [8](E08.java) | 9  | 10 |
+| :heavy_check_mark: [6](E06.java) | :heavy_check_mark: [7](E07.java) | :heavy_check_mark: [8](E08.java) | :heavy_check_mark: [9](E09.java)  | 10 |
 | 11 | 12 | 13 | 14 | 15 |
 | 16 | 17 | 18 | 19 | 20 |
