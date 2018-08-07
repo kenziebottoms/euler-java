74fb0856b3bec8b57fc7e94d61cf0e6fcef93f4a
kenziebottoms <kenziebottoms@gmail.com>
2018-08-07 10:56:01 -0500

    solved #10 in java 6

--- 6/E10.java
+++ 6/E10.java
@@ -2,24 +2,37 @@ import java.util.*;
 
 class E10 {
     public static void main(String[] args) {
-        System.out.println(primes(10));
+        System.out.println(sum(primesUpTo(2000000)));
     }
 
-    public static List<Long> primes(int terms) {
+    public static List<Long> primesUpTo(Long limit) {
         List<Long> primes = new ArrayList<Long>();
-        primes.add(new Long(2));
+        primes.add(new Long(3));
         Long i = new Long(3);
-        while(primes.size() < terms) {
+        while(i < limit) {
             for (Long prime: primes) {
                 if (i % prime == 0) {
                     break;
-                } else {
+                } else if (prime > Math.sqrt(i) || primes.get(primes.size()-1) == prime) {
                     primes.add(i);
                     break;
                 }
             }
-            i += 1;
+            i += 2;
         }
+        primes.add(0, new Long(2));
         return primes;
     };
+    public static List<Long> primesUpTo(int limit) {
+        return primesUpTo(new Long(limit));
+    };
+
+    // returns sum of array
+    public static Long sum(List<Long> arr) {
+        Long sum = new Long(0);
+        for (int i=0; i<arr.size(); i++) {
+            sum += arr.get(i);
+        }
+        return sum;
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Project Euler: Java
 
-- [1.6](6) (8 solved)
+- [1.6](6) (10 solved)
 - [1.8](8) (1 solved)
